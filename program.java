be8dabc36bfbced3181a485e75dd9f8a81f07414
Andrej-Petru <petruse2@uwindsor.ca>
2025-11-28 14:37:23 -0500

    normalized files, now need to find a way to save them

--- program.java
+++ program.java
@@ -1,10 +1,10 @@
-import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 
 public class program {
-    public static void main(){
+    public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a file to scan");
@@ -17,10 +17,22 @@ public class program {
         try (Scanner myReader = new Scanner(newFile)) {
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                System.out.println(data);
+                String normalized = data.replaceFirst("\\s+", "");
+                System.out.println(normalized);
             }
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        try (Scanner myReader = new Scanner(oldFile)){
+            while (myReader.hasNextLine()){
+                String data = myReader.nextLine();
+                String normalized = data.replaceFirst("\\s+", "");
+                System.out.println(normalized);
+            }
+        }catch (FileNotFoundException e){
+            System.out.println("error");
             e.printStackTrace();
         }
     }
