daea6f5b07f4610f88f0dd1eab6e09545a1b5089
Andrej-Petru <petruse2@uwindsor.ca>
2025-12-01 17:42:15 -0500

    cosine and final answer
    need to make a function with candidateline

--- src/lp-diff/core/similarity.py
+++ src/lp-diff/core/similarity.py
@@ -1,5 +1,8 @@
 import Levenshtein
 import sklearn
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 
 def hamming_distance(a:int, b:int) ->int:
@@ -11,3 +14,13 @@ def levenshtein_similarity(s: str, t:str) ->float:
     dist = Levenshtein.distance(s,t)
     max_len = max(len(s), len(t))
     return 1.0-dist/max_len
+
+def calculate_cosine(contextline1, contextline2):
+    tfdif = TfidfVectorizer()
+    vecs = tfdif.fit_transform([contextline1, contextline2])
+    score = cosine_similarity(vecs[0], vecs[1])[0][0]
+    return score
+
+def combinedscore(line1,line2,contextline1,context1,context2):
+    return levenshtein_similarity(line1,line2)*0.6 + 0.4*calculate_cosine(context1,context2)
+    
